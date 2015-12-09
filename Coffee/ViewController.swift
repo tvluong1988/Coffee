d08efee0f2e3a93938d2a18ffb94b93f11fd84bb
Thinh Luong <guitarslacker@gmail.com>
2015-12-08 21:57:42 -0600

    Initial Commit

---
+++ Coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coffee
+//
+//  Created by Thinh Luong on 12/8/15.
+//  Copyright © 2015 Thinh Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
